d1f3ca5a05d3f83b308290927fe0bbfbbbbf7856
Nuno <nuno@studiographene.com>
2024-05-27 17:53:03 +0100

    react router dom

--- nodependents/src/views/consumer/ConsumerDashboard.jsx
+++ nodependents/src/views/consumer/ConsumerDashboard.jsx
@@ -1,27 +1,29 @@
 import { TextField } from "@mui/material";
+import { Link } from "react-router-dom";
 import { SERVICES } from "../../utils/constants";
-import './styles.css';
+import "./styles.css";
 
 const ConsumerDashboard = () => {
-    return (
-      <div style={{padding: '20px'}}>
-        <header>
-          <h1>Consumer Dashboard</h1>
-        </header>
-        <div>
-          <TextField id="outlined-basic" label="Search" variant="outlined" />
-        </div>
-        <div>
-          {SERVICES.map((service, index) => (
-            <div key={index}>
+  return (
+    <div style={{ padding: "20px" }}>
+      <header>
+        <h1>Consumer Dashboard</h1>
+      </header>
+      <div>
+        <TextField id="outlined-basic" label="Search" variant="outlined" />
+      </div>
+      <div>
+        {SERVICES.map((service, index) => (
+          <div key={index}>
+            <Link to={`/consumer/booking/transportation/date-selection`}>
               <h2>{service.name}</h2>
               <p>{service.description}</p>
-            </div>
-          ))}
-        </div>
+            </Link>
+          </div>
+        ))}
       </div>
-    );
-  };
-  
-  export default ConsumerDashboard;
-  
+    </div>
+  );
+};
+
+export default ConsumerDashboard;
