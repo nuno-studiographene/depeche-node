d9fe1dadc0300dda70eb9961ec4bc9e185e949ed
Joao Nacho <joao.nacho@studiographene.com>
2024-05-27 15:56:47 +0100

    reset css, constant utils

--- nodependents/src/views/signup/worker/index.jsx
+++ nodependents/src/views/signup/worker/index.jsx
@@ -1,9 +1,30 @@
 const WorkerSignup = () => {
+  const workerServices = [
+    { name: "Pharmacy", value: "pharmacy" },
+    { name: "Ambulance", value: "ambulance" },
+    { name: "Health care assistant", value: "health-care-assistant" },
+    { name: "At Home Care", value: "at-home-care" },
+    { name: "Physical Treatment", value: "physical-treatment" },
+    { name: "Geriatric Care", value: "geriatric-care" },
+    { name: "Mental Health Services", value: "mental-health-services" },
+    { name: "Dental Care", value: "dental-care" },
+    { name: "Optometry", value: "optometry" },
+    { name: "Chiropractic Services", value: "chiropractic-services" },
+  ];
   return (
     <div>
       <header>
         <h1>WORKER SIGNUP</h1>
       </header>
+      <main>
+        <section>
+          <form>
+            {workerServices.map((service) => (
+              <div style={{ width: "50px", height: "50px" }}></div>
+            ))}
+          </form>
+        </section>
+      </main>
     </div>
   );
 };
